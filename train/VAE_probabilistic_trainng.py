083af72c298f37b7ed50cc361554da1d0f7f91b6
jdsalamanca <32911041+jdsalamanca@users.noreply.github.com>
2023-09-21 19:34:02 -0500

    Create VAE_probabilistic_trainng.py

---
+++ train/VAE_probabilistic_trainng.py
@@ -0,0 +1,3 @@
+# Training with probability densities instead of reconstruction error (MSE) as performed by Jinwon and Sungzoon in their paper "Variational Autoencoder based Anomaly Detection
+using Reconstruction Probability"
+
